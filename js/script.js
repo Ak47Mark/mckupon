10ee3ff5f07559a4ed70907665286541ac7c02e0
Varga Márk <ak47mark@gmail.com>
2023-01-18 15:14:53 +0100

    Új oldal megnyitása

--- js/script.js
+++ js/script.js
@@ -3,3 +3,13 @@ $(document).ready(function () {
         console.log($(this).val());
     });
 });
+
+$('#code-bt').on('click', function(){
+    var value = $("#code").val();
+    chrome.storage.local.set({"code": value}, function(){
+        var newURL = "https://mcdonalds.fast-insight.com/voc/hu/hu";
+        var tab = chrome.tabs.create({ url: newURL },function(){
+            console.log(tab);
+        });
+    });
+});
